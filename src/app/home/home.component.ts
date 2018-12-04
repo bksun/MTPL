77862fcec1e4e9df4c1822c4fa96f11e98e32d63
bksun <sunilsinghmbm@gmail.com>
2018-12-04 06:11:35 +0530

    add home comoffices with icons on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,12 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   products = [];
+  cities = [];
 
   constructor() { }
 
   ngOnInit() {
     this.products = ['buterfly.jpg', 'bars2.png', 'fittings.jpg', 'volves.jpg', 'pipes2.jpg', 'flanges.jpg'
-    , 'stainless-steel-sheet.jpg', 'poles.jpg'];
+    , 'stainless-steel-sheet.jpg', 'poles.jpg', 'fasteners2.jpg'];
+
+    this.cities = [
+      { img: 'ahmedabad.jpg', place: 'Ahmedabad' },
+      { img: 'bangalore.png', place: 'Bangalore' },
+      { img: 'chennai.png', place: 'Chennai' },
+      { img: 'delhi.png', place: 'Delhi' },
+      { img: 'ghaziabad.png', place: 'Ghaziabad' },
+      { img: 'gurgaon.png', place: 'Gurgaon' },
+      { img: 'hyderabad.png', place: 'Hyderabad' },
+      { img: 'jaipur.png', place: 'Jaipur' },
+      { img: 'jalandhar.png', place: 'Jalandhar' },
+      { img: 'kolkata.png', place: 'Kolkata' },
+      { img: 'mumbai.png', place: 'Mumbai' },
+      { img: 'raipur.png', place: 'Raipur' }];
   }
+
 
 }
