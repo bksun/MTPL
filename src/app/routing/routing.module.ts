0682a01467364216fad8ff4ff499f3ca2ce75932
bksun <sunilsinghmbm@gmail.com>
2018-12-08 21:59:43 +0530

    add contact page with all locations

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -4,11 +4,12 @@ import { HomeComponent } from '../home/home.component';
 import { AboutComponent } from '../about/about.component';
 import { ManagementComponent } from '../management/management.component';
 import { SidenavMainComponent } from '../sidenav-main/sidenav-main.component';
+import { ContactComponent } from '../contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'contact', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
   { path: 'about', component: AboutComponent},
   { path: 'management', component: ManagementComponent},
   { path: 'sidenav', component: SidenavMainComponent }
