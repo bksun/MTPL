26f0845e95e7c6748f9389bd57627dc21ab00f09
bksun <sunilsinghmbm@gmail.com>
2018-12-10 22:54:40 +0530

    debug 404 Not Found Refresh Issue

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -3,16 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { AboutComponent } from '../about/about.component';
 import { ManagementComponent } from '../management/management.component';
-import { SidenavMainComponent } from '../sidenav-main/sidenav-main.component';
+// import { SidenavMainComponent } from '../sidenav-main/sidenav-main.component';
 
-import {CertificateComponent} from '../certificate/certificate.component';
+import { CertificateComponent } from '../certificate/certificate.component';
 import { ContactComponent } from '../contact/contact.component';
 import { ProgressComponent } from '../progress/progress.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-   { path: 'contact', component: ContactComponent },
+  { path: 'contact', component: ContactComponent },
   { path: 'about', component: AboutComponent},
   { path: 'management', component: ManagementComponent},
   { path: 'sidenav', component: ProgressComponent },
@@ -22,7 +22,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes,  { useHash: true })
   ],
   exports: [RouterModule]
 })
